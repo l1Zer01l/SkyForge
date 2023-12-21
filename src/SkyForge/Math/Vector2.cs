b19db85dac32c2250f09307c99a14a2160e5f498
Hard_Code <zero-0_0@bk.ru>
2023-12-21 18:43:34 +0500

    Added Vector2

---
+++ src/SkyForge/Math/Vector2.cs
@@ -0,0 +1,45 @@
+
+namespace SkyForge.Math
+{
+    public class Vector2
+    {
+        public float x;
+        public float y;
+
+        public Vector2() : this(0.0f, 0.0f) { }
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Normalized()
+        {
+            var lenghtVector = (float)System.Math.Sqrt(x * x + y * y);
+            x /= lenghtVector;
+            y /= lenghtVector;
+        }
+
+        public override string ToString()
+        {
+            return $"x:{x} y:{y}";
+        }
+
+        public static Vector2 operator +(Vector2 vector1, Vector2 vector2)
+        {
+            return new Vector2(vector1.x + vector2.x, vector1.y + vector2.y);
+        }
+
+        public static Vector2 operator -(Vector2 vector1, Vector2 vector2)
+        {
+            return new Vector2(vector1.x + vector2.x, vector1 .y - vector2.y);
+        }
+
+        public static Vector2 operator *(Vector2 vector, float value)
+        {
+            return new Vector2(vector.x * value, vector.y * value);
+        }
+
+    }
+
+}
